8011c0112700bc7aafa005e0cbedc7a4e9b541d6
Sydney Pogue <sp1441@Sydneys-MacBook-Air.local>
2023-08-10 17:58:31 -0400

    converted tasks to doses and added JWT

--- src/app/components/calendar/Calender.js
+++ src/app/components/calendar/Calender.js
@@ -8,6 +8,7 @@ const DAYS_OF_WEEK = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
 export default function CustomCalendar({ calendarData, tasks = [] }) {
     const [currentMonth, setCurrentMonth] = useState(new Date());
+
     const isTaskForDay = (day, tasks, currentMonth) => {
         return tasks.some(task => {
             const taskDate = new Date(task.date);
@@ -17,15 +18,30 @@ export default function CustomCalendar({ calendarData, tasks = [] }) {
         });
     };
 
-
     useEffect(() => {
-        axios.get(`${process.env.NEXT_PUBLIC_SERVER_URL}/sanctuary/tasks`)
+        const token = localStorage.getItem('jwtToken');
+        if (!token) {
+            console.log("No token found. Please log in again.");
+            // maybe redirect to login page or handle appropriately
+            return;
+        }
+
+        axios.get(`${process.env.NEXT_PUBLIC_SERVER_URL}/doses`, { headers: { 'Authorization': `Bearer ${token}` } })
             .then(response => {
-                const pendingTasks = response.data.filter(task => task.status === 'pending');
-                setTasks(pendingTasks);
+                const userId = response.data.userId; // assuming JWT payload contains userId
+
+                // filter doses for this user which haven't been taken.
+                const upcomingDoses = response.data.filter(dose => dose.user === userId && dose.taken === false);
+
+                setTasks(upcomingDoses);
             })
             .catch(error => {
-                console.log('Error fetching tasks data: ', error);
+                console.log('Error fetching doses data: ', error);
+                if (error.response && (error.response.status === 403 || error.response.status === 500)) {
+                    console.log("Token might be expired or invalid. Please log in again.");
+                    // Optionally remove invalid token and/or redirect user to login page
+                    localStorage.removeItem('jwtToken');
+                }
             });
     }, []);
 
@@ -93,5 +109,4 @@ export default function CustomCalendar({ calendarData, tasks = [] }) {
             </table>
         </div>
     );
-
 }
