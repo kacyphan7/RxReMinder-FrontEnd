d2e07bf8624b86fd40428184dbab811d36849645
Sydney Pogue <sp1441@Sydneys-MacBook-Air.local>
2023-08-10 01:15:46 -0400

    changed around layout to stretch navbar column to full height of page

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -6,38 +6,39 @@ import CustomCalendar from '../components/calendar/Calender';
 function Dashboard() {
     return (
         <div className="container">
-            <div className="level">
-                {/* user greeting */}
-                <div className="level-left">
-                    <h1 className="title is-2">Hello, [User's Name]!</h1>
-                </div>
-
-                {/* profile image icon on the top right */}
-                <div className="level-right">
-                    <figure className="image is-48x48">  {/* You can adjust the size as needed */}
-                        <img src="/path-to-user-profile-icon.jpg" alt="User's Profile" />
-                    </figure>
-                </div>
-            </div>
-
             <div className="columns">
-                {/* FIRST COLUMN */}
-                <div className="column is-one-quarter">
-                    <div className="card">
+                {/* FIRST COLUMN (Navbar) */}
+                <div className="column is-one-quarter navbar-column"> {/* Added navbar-column for custom styling */}
+                    <div className="card full-height"> {/* Added full-height class for custom styling */}
                         <div className="card-content">
                             {/* <navbar /> */}
+                            <div>Placeholder for navbar card</div>
                         </div>
                     </div>
                 </div>
 
                 {/* SECOND COLUMN */}
                 <div className="column is-half">
+                    <div className="level">
+                        {/* user greeting */}
+                        <div className="level-left">
+                            <h1 className="title is-2">Hello, [User's Name]!</h1>
+                        </div>
+
+                        {/* profile image icon on the top right */}
+                        <div className="level-right">
+                            <figure className="image is-48x48">  {/* You can adjust the size as needed */}
+                                <img src="/path-to-user-profile-icon.jpg" alt="User's Profile" />
+                            </figure>
+                        </div>
+                    </div>
+
                     <div className="card">
                         <div className="card-content">
                             <CustomCalendar />
                         </div>
                     </div>
-
+                    <br />
                     <div className="columns"> {/* Nested columns */}
                         {/* First card */}
                         <div className="column">
@@ -74,4 +75,5 @@ function Dashboard() {
 }
 
 export default Dashboard;
+
 
