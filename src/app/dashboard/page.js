40da61eb0242100fdef35123cc43b9e488c39d0c
Sydney Pogue <sp1441@Sydneys-MacBook-Air.local>
2023-08-10 00:59:09 -0400

    split cards in second column and relabeled columns

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -7,18 +7,21 @@ function Dashboard() {
     return (
         <div className="container">
             <div className="level">
+                {/* user greeting */}
                 <div className="level-left">
                     <h1 className="title is-2">Hello, [User's Name]!</h1>
                 </div>
+
+                {/* profile image icon on the top right */}
                 <div className="level-right">
-                    <figure className="image is-48x48">
+                    <figure className="image is-48x48">  {/* You can adjust the size as needed */}
                         <img src="/path-to-user-profile-icon.jpg" alt="User's Profile" />
                     </figure>
                 </div>
             </div>
 
             <div className="columns">
-                {/* first column - Navbar in a card */}
+                {/* FIRST COLUMN */}
                 <div className="column is-one-quarter">
                     <div className="card">
                         <div className="card-content">
@@ -27,7 +30,7 @@ function Dashboard() {
                     </div>
                 </div>
 
-                {/* second column - calendar and filler components in cards */}
+                {/* SECOND COLUMN */}
                 <div className="column is-half">
                     <div className="card">
                         <div className="card-content">
@@ -35,15 +38,28 @@ function Dashboard() {
                         </div>
                     </div>
 
-                    <div className="card">
-                        <div className="card-content">
-                            {/* <component placeholder /> */}
-                            <div>Placeholder content for second column</div>
+                    <div className="columns"> {/* Nested columns */}
+                        {/* First card */}
+                        <div className="column">
+                            <div className="card">
+                                <div className="card-content">
+                                    <div>Placeholder content for first card</div>
+                                </div>
+                            </div>
+                        </div>
+
+                        {/* Second card */}
+                        <div className="column">
+                            <div className="card">
+                                <div className="card-content">
+                                    <div>Placeholder content for second card</div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
 
-                {/* Third column - Placeholder in a card */}
+                {/* THIRD COLUMN */}
                 <div className="column is-one-quarter">
                     <div className="card">
                         <div className="card-content">
@@ -58,3 +74,4 @@ function Dashboard() {
 }
 
 export default Dashboard;
+
