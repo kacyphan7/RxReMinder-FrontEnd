ae1872fe9a494eace0ef3fa23d993819bf10eaf2
Sydney Pogue <sp1441@Sydneys-Air.lan>
2023-08-12 14:21:58 -0400

    inserted a return

--- src/app/components/daily percentage/DailyPercentage.js
+++ src/app/components/daily percentage/DailyPercentage.js
@@ -2,7 +2,7 @@
 
 import React, { useState, useEffect } from 'react';
 
-function DailyPercentage() {
+export default function DailyPercentage() {
     const [percentage, setPercentage] = useState(null);
 
     useEffect(() => {
@@ -23,6 +23,23 @@ function DailyPercentage() {
         fetchData();
     }, []);
 
-    // Render the component
-    // ...
+    const mockdata = [
+        {
+            name: 'Percentage',
+            uv: percentage,
+            fill: '#8884d8'
+        }
+    ];
+
+    return (
+        <div className="container">
+            {percentage !== null && (
+                <RadialBarChart width={300} height={300} innerRadius={20} outerRadius={140} barSize={10} data={data}>
+                    <RadialBar minAngle={15} background clockWise dataKey="uv" />
+                    <Legend iconSize={10} width={120} height={140} layout="vertical" verticalAlign="middle" wrapperStyle={{ top: 0, right: 0, lineHeight: '24px' }} />
+                    <Tooltip />
+                </RadialBarChart>
+            )}
+        </div>
+    );
 }
