79ab3208c2f2fc9918f7fbfab69de937cc7b844a
ellehcim burton <ellehcimburton@ellehcims-MBP.lan>
2023-08-11 22:46:04 -0400

    feat: style register page

--- src/app/components/register/Register.js
+++ src/app/components/register/Register.js
@@ -3,8 +3,24 @@
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import axios from 'axios';
+import fa from'src/app/assets/fontawesome.js';
+import brands from 'src/app/assets/brands.js';
+import solid from 'src/app/assets/solid.js';
+import Image from 'next/image';
+import pillBox from 'src/app/assets/pillBox.jpg'
 
 export default function Register() {
+    const overlayStyle = {
+        // marginTop: '200px',
+        // marginLeft: '270px',
+        // textShadow: '5px 5px 5px ',
+        // position: 'absolute'
+    }
+
+    const containerStyle = {
+        marginTop: '100px',
+    }
+
 	const router = useRouter();
 	const [redirect, setRedirect] = useState(false);
 
@@ -81,50 +97,89 @@ export default function Register() {
 	return (
         <>
             {error ? <p className="error">Email already exists.</p> : null}
-            <h1>Register</h1>
-            <form onSubmit={handleSubmit}>
-                <div className="field">
-                    <label htmlFor="email">Email</label>
-                    <div className="control">
-                        <input className="input" name="email" value={email} onChange={handleEmail} type="email" placeholder="Email (required)" required />
-                    </div>
+            
+        
+		<div style={containerStyle} className="hero">
+			<div className="container has-text-centered is-full-height">
+				<div className="columns is-7 is-variable box">
+					<div className="column is-two-thirds has-text-left">
+                        <div>
+                            <Image src={pillBox}
+                                height={400}
+                                width={800}
+                            />
+                        </div>
+                        {/* <div className='is-overlay'> */}
+						<div style={overlayStyle} className="social-media is-relative">
+                            <h1 className="title is-1">RxReMinder</h1>
+                        </div>
+                        {/* </div> */}
+					</div>
+                    <div className='column'>
+                    <form onSubmit={handleSubmit}>
+                        <div className="field">
+                            <label htmlFor="email">Email</label>
+                            <div className="control has-icons-right">
+                                <input className="input is-rounded" name="email" value={email} onChange={handleEmail} type="email" placeholder="Email (required)" required />
+                                <span className="icon is-small is-right">
+                                <i className="fas fa-envelope"></i>
+                                </span>
+                            </div>
+                        </div>
+                        <div className="field">
+                            <label htmlFor="password">Password</label>
+                            <div className="control has-icons-right">
+                                <input className="input is-rounded" name="password" value={password} onChange={handlePassword} type="password" placeholder="Password (required)" required />
+                                <span className="icon is-small is-right">
+                                <i className="fa-solid fa-key"></i>
+                                </span>
+                            </div>
+                        </div>
+                        <div className="field">
+                            <label htmlFor="firstName">First Name</label>
+                            <div className="control has-icons-right">
+                                <input className="input is-rounded" name="firstName" value={firstName} onChange={handleFirstName} type="text" placeholder="First Name (required)" required />
+                                <span className="icon is-small is-right">
+                                <i className="fa-solid fa-user"></i>
+                                </span>
+                            </div>
+                        </div>
+                        <div className="field">
+                            <label htmlFor="lastName">Last Name</label>
+                            <div className="control has-icons-right">
+                                <input className="input is-rounded" name="lastName" value={lastName} onChange={handleLastName} type="text" placeholder="Last Name (required)" required />
+                                <span className="icon is-small is-right">
+                                <i className="fa-solid fa-user"></i>
+                                </span>
+                            </div>
+                        </div>
+                        <div className="field">
+                            <label htmlFor="birthdate">Birthdate</label>
+                            <div className="control has-icons-right">
+                                <input className="input is-rounded" name="birthdate" value={birthdate} onChange={handleBirthdate} type="date" required />
+                            </div>
+                        </div>
+                        <div className="field">
+                            <label htmlFor="phoneNumber">Phone Number</label>
+                            <div className="control has-icons-right">
+                                <input className="input is-rounded" name="phoneNumber" value={phoneNumber} onChange={handlePhoneNumber} type="tel" placeholder="Phone Number" />
+                                <span className="icon is-small is-right">
+                                <i className="fa-solid fa-phone"></i>
+                                </span>
+                            </div>
+                        </div>
+                        <div className="field">
+                            <div className="control has-icons-right">
+                                <button className="button is-rounded is-fullwidth" type='submit'>Register</button>
+                            </div>
+                            
+                        </div>
+                    </form>
+				</div>
+		            <span className='icon is-right'><a href='/'><i class="fa-solid fa-person-walking-arrow-loop-left fa-xl"></i></a></span>
                 </div>
-                <div className="field">
-                    <label htmlFor="password">Password</label>
-                    <div className="control">
-                        <input className="input" name="password" value={password} onChange={handlePassword} type="password" placeholder="Password (required)" required />
-                    </div>
-                </div>
-                <div className="field">
-                    <label htmlFor="firstName">First Name</label>
-                    <div className="control">
-                        <input className="input" name="firstName" value={firstName} onChange={handleFirstName} type="text" placeholder="First Name (required)" required />
-                    </div>
-                </div>
-                <div className="field">
-                    <label htmlFor="lastName">Last Name</label>
-                    <div className="control">
-                        <input className="input" name="lastName" value={lastName} onChange={handleLastName} type="text" placeholder="Last Name (required)" required />
-                    </div>
-                </div>
-                <div className="field">
-                    <label htmlFor="birthdate">Birthdate</label>
-                    <div className="control">
-                        <input className="input" name="birthdate" value={birthdate} onChange={handleBirthdate} type="date" required />
-                    </div>
-                </div>
-                <div className="field">
-                    <label htmlFor="phoneNumber">Phone Number</label>
-                    <div className="control">
-                        <input className="input" name="phoneNumber" value={phoneNumber} onChange={handlePhoneNumber} type="tel" placeholder="Phone Number" />
-                    </div>
-                </div>
-                <div className="field">
-                    <div className="control">
-                        <button className="button" type='submit'>Register</button>
-                    </div>
-                </div>
-            </form>
+				</div>
+			</div>
         </>
 	);
 };
