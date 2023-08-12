38230df9bf260fb7c632a178150fae95d5cc29b1
Sydney Pogue <sp1441@Sydneys-Air.lan>
2023-08-12 15:48:22 -0400

    imported c3js and used it for dailypercentage component, inserted fake data

--- src/app/components/daily percentage/DailyPercentage.js
+++ src/app/components/daily percentage/DailyPercentage.js
@@ -1,50 +1,54 @@
 "use client";
 
-import React, { useState, useEffect } from 'react';
-import { RadialBarChart, RadialBar, Legend, Tooltip } from 'recharts';
+import React, { useEffect } from 'react';
+import c3 from 'c3';
+import 'c3/c3.css'; // Importing styles for c3
 
-export default function DailyPercentage() {
-    // Use the mock data directly for testing.
-    const percentage = 75;  // This represents 75%. You can adjust this value to test different percentages.
-
-    /*
-    // Commenting out the useState and useEffect parts related to fetching data.
-    const [percentage, setPercentage] = useState(null);
-
+const DailyPercentage = () => {
     useEffect(() => {
-        async function fetchData() {
-            try {
-                const response = await fetch('http://localhost:8000/doses/dailypercentage', {
-                    method: 'GET',
-                    headers: {
-                        'Authorization': 'Bearer ' + INSERT_JWT_TOKEN,
-                    }
-                });
-                const data = await response.json();
-                setPercentage(data);
-            } catch (error) {
-                console.error('There was an issue fetching the daily percentage:', error);
+        const chart = c3.generate({
+            bindto: '#daily-percentage-chart',
+            data: {
+                columns: [
+                    ['Remaining', 100],  // start with 100
+                    ['Percentage', 0],  // start with 0
+                ],
+                type: 'donut',
+                order: null,
+                colors: {
+                    Percentage: '#8884d8',
+                    Remaining: '#f4f4f4'
+                }
+            },
+            donut: {
+                title: "10%",
+                width: 30,
+                startingAngle: 1.5 * Math.PI // starts animation from top
+            },
+            transition: {
+                duration: 1500
             }
-        }
-        fetchData();
-    }, []);
-    */
+        });
 
-    const data = [
-        {
-            name: 'Percentage',
-            uv: percentage,
-            fill: '#8884d8'
-        }
-    ];
+        // introducing a delay before loading actual data to simulate animation
+        setTimeout(() => {
+            chart.load({
+                columns: [
+                    ['Remaining', 90],
+                    ['Percentage', 10]
+                ]
+            });
+        }, 100); // delay of 100ms
+
+        // cleanup on unmount
+        return () => {
+            chart.destroy();
+        };
+    }, []);
 
     return (
-        <div className="container">
-            <RadialBarChart width={300} height={300} innerRadius={60} outerRadius={180} barSize={20} data={data}>
-                <RadialBar minAngle={15} maxAngle={360} background clockWise={true} dataKey="uv" />
-                <Legend iconSize={10} width={120} height={140} layout="vertical" verticalAlign="middle" wrapperStyle={{ top: 0, right: 0, lineHeight: '24px' }} />
-                <Tooltip />
-            </RadialBarChart>
-        </div>
+        <div id="daily-percentage-chart"></div>
     );
 }
+
+export default DailyPercentage;
