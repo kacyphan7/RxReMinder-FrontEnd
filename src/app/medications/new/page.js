afa7844733f119e37a52b1824f2156f3e7fff260
darkartaudio <mikey@mikeyallred.com>
2023-08-13 11:06:49 -0500

    feat: new medication page template with sidebar

---
+++ src/app/medications/new/page.js
@@ -0,0 +1,54 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
+import jwtDecode from 'jwt-decode';
+import setAuthToken from '@/app/utils/setAuthToken';
+import handleLogout from '@/app/utils/handleLogout';
+
+import Layout from '@/app/components/sidebar/SideBar';
+
+export default function NewMedicationPage() {
+    const router = useRouter();
+    const [data, setData] = useState(null);
+    const [isLoading, setLoading] = useState(true);
+
+    if (typeof window !== 'undefined') {
+        const expirationTime = new Date(localStorage.getItem('expiration') * 1000);
+        let currentTime = Date.now();
+
+        if (currentTime >= expirationTime) {
+            handleLogout();
+            router.push('/login');
+        }
+    }
+
+    useEffect(() => {
+        setAuthToken(localStorage.getItem('jwtToken'));
+        if (localStorage.getItem('jwtToken')) {
+            axios.get(`${process.env.NEXT_PUBLIC_SERVER_URL}/users/email/${localStorage.getItem('email')}`)
+                .then((response) => {
+                    let userData = jwtDecode(localStorage.getItem('jwtToken'));
+                    if (userData.email === localStorage.getItem('email')) {
+                        setData(response.data.users[0]);
+                        setLoading(false);
+                    } else {
+                        router.push('/login');
+                    }
+                })
+                .catch((error) => {
+                    console.log(error);
+                    router.push('/login');
+                });
+        } else {
+            router.push('/login');
+        }
+    }, [router]);
+
+    return (
+        <Layout>
+            
+        </Layout>
+    );
+}
