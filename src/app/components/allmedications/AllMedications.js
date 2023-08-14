43e2017ae0c1d489e73ee910f6f1a0f7a9de7ffc
Sydney Pogue <sp1441@Sydneys-Air.lan>
2023-08-14 00:18:59 -0400

    filled in auth token logic

--- src/app/components/allmedications/AllMedications.js
+++ src/app/components/allmedications/AllMedications.js
@@ -2,17 +2,34 @@
 
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import 'bulma/css/bulma.css';
+import jwtDecode from 'jwt-decode';
+import { useRouter } from 'next/router';
+import handleLogout from '@/app/utils/handleLogout';
+import setAuthToken from '@/app/utils/setAuthToken';
 
 function AllMedications() {
     const [medications, setMedications] = useState([]);
+    const router = useRouter();
+
+    if (typeof window !== 'undefined') {
+        const expiration = new Date(localStorage.getItem('expiration') * 1000);
+        let currentTime = Date.now();
+
+        if (currentTime > expiration) {
+            handleLogout();
+            router.push('/');
+        }
+    }
 
     useEffect(() => {
+        setAuthToken(localStorage.getItem('jwtToken'));
+
         async function fetchMedications() {
             try {
                 const response = await axios.get('http://localhost:8000/medications/user', {
-                    // Pass the JWT token if required for authentication
                     headers: {
-                        Authorization: 'Bearer ' + YOUR_JWT_TOKEN
+                        Authorization: 'Bearer ' + localStorage.getItem('jwtToken')
                     }
                 });
 
@@ -22,8 +39,19 @@ function AllMedications() {
             }
         }
 
-        fetchMedications();
-    }, []);
+        if (localStorage.getItem('jwtToken')) {
+            let userData = jwtDecode(localStorage.getItem('jwtToken'));
+
+            if (userData.email === localStorage.getItem('email')) {
+                fetchMedications();
+            } else {
+                router.push('/');
+            }
+        } else {
+            router.push('/');
+        }
+
+    }, [router]);
 
     return (
         <div className="all-medications">
