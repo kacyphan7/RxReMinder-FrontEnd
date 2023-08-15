61e720e40880fe7ba9a70037fad872e9c4860c18
darkartaudio <mikey@mikeyallred.com>
2023-08-14 23:43:12 -0500

    feat: very minimal styling on MedicationsWidget, added medication category

--- src/app/components/medicationswidget/MedicationsWidget.js
+++ src/app/components/medicationswidget/MedicationsWidget.js
@@ -1,81 +1,42 @@
 "use client";
 
 import React, { useEffect, useState } from 'react';
-import { useRouter } from 'next/navigation';
-import jwtDecode from 'jwt-decode';
-import setAuthToken from '@/app/utils/setAuthToken';
-import handleLogout from '@/app/utils/handleLogout';
 import axios from 'axios';
 
+import styles from "@/app/css/medications-widget.module.css";
+
 export default function MedicationsWidget() {
-    const router = useRouter();
-    const [user, setUser] = useState(null);
     const [medications, setMedications] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
-
-
-    // Handle session expiration logic
-    if (typeof window !== 'undefined') {
-        const expiration = new Date(localStorage.getItem('expiration') * 1000);
-        let currentTime = Date.now();
-
-        if (currentTime > expiration) {
-            handleLogout();
-            router.push('/');
-        }
-    }
-
     useEffect(() => {
-        setAuthToken(localStorage.getItem('jwtToken'));
-        if (localStorage.getItem('jwtToken')) {
-            axios.get(`${process.env.NEXT_PUBLIC_SERVER_URL}/users/email/${localStorage.getItem('email')}`)
-                .then((response) => {
-                    let userData = jwtDecode(localStorage.getItem('jwtToken'));
-                    if (userData.email === localStorage.getItem('email')) {
-                        setUser(response.data.users[0]);
-                        setLoading(false);
-                    } else {
-                        router.push('/');
-                    }
-                })
-                .catch((err) => {
-                    console.log(err);
-                    router.push('/');
-                });
-        } else {
-            router.push('/');
-        }
-    }, [router]);
+        axios.get(`${process.env.NEXT_PUBLIC_SERVER_URL}/medications/user`)
+            .then(response => {
+                setMedications(response.data);
+                setLoading(false);
+            })
+            .catch(err => {
+                console.error("There was an error fetching the medications data: ", err);
+                setError(err);
+            });
+    }, [loading]); // Add user as a dependency
 
-    useEffect(() => {
-        if (!loading && user) { // mkes sure that the user data is fetched before trying to fetch medications
-            axios.get(`${process.env.NEXT_PUBLIC_SERVER_URL}/medications/user`)
-                .then(response => {
-                    setMedications(response.data);
-                    setLoading(false);
-                })
-                .catch(err => {
-                    console.error("There was an error fetching the medications data: ", err);
-                    setError(err);
-                    setLoading(false);
-                });
-        }
-    }, [loading, user]); // Add user as a dependency
-
-    if (loading) return <div>Loading...</div>;
     if (error) return <div>Error: {error.message}</div>;
+    if (loading) return <div>Loading...</div>;
 
     return (
         <div>
-            {medications.map(medication => (
-                <div key={medication._id} className="card">
-                    <div className="card-content has-text-centered">
-                        <p className="title">{medication.name}</p>
+            <div className={styles.maxHeight}>
+                {medications.map(medication => (
+                    <div key={medication._id} className="card">
+                        <div className="card-content has-text-centered">
+                            <p className="title is-6">{medication.name}</p>
+                            <p className="subtitle is-6">{medication.category}</p>
+                        </div>
                     </div>
-                </div>
-            ))}
+                ))}
+            </div>
         </div>
     );
 }
